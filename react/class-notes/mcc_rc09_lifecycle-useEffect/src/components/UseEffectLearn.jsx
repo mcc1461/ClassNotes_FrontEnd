4860251cd1bba20dc60e90b801695743950130a9
mcc1461 <mcoskuncelebi@gmail.com>
2023-12-17 10:37:21 +0300

    .

--- react/class-notes/mcc_rc09_lifecycle-useEffect/src/components/UseEffectLearn.jsx
+++ react/class-notes/mcc_rc09_lifecycle-useEffect/src/components/UseEffectLearn.jsx
@@ -4,16 +4,13 @@ import { useEffect, useState } from "react";
 const UseEffectLearn = () => {
   const [name, setName] = useState(" ");
   const [surname, setSurname] = useState(" ");
-
-
+  const [tittle, setTittle] = useState("Learn React");
   useEffect(() => {
     console.log("useEffect çalıştı");
   });
-
   return (
     <>
       <div className="">
-
         <label htmlFor="name" className="fw-bold">Name:</label>
         <input id="name" className="px-2 bg-success fs-2 text-light mt-1" type="text" onChange={(e) => setName(e.target.value)} />
        <br />
@@ -23,10 +20,15 @@ const UseEffectLearn = () => {
       </div>
       <h2 className="bg-warning w-25 rounded ">{name}</h2>
       <h2 className="bg-warning w-25 rounded ">{surname}</h2>
-
       <h2>Merhaba Arkadaşlar...</h2>
     </>
   );
 };
+function Container () {
+  return (
+    <div style={{width:"400px", height:"400px", border:"2px solid red"}}>
+    </div>
+  )
+}
 
 export default UseEffectLearn;
