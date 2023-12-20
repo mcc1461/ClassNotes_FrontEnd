c76615914091565ad023b117bb83e8c80ffc0b1b
mcc1461 <mcoskuncelebi@gmail.com>
2023-12-20 20:48:41 +0300

    sizing

--- react/class-notes/mcc_rc13_tailwind/src/components/Typography.jsx
+++ react/class-notes/mcc_rc13_tailwind/src/components/Typography.jsx
@@ -11,6 +11,25 @@ const Typography = () => {
       <h2 className="text-2xl">Typography</h2>
       <h3 className="text-2xl">Typography</h3>
       <h1 className="text-[40px]">Typography</h1>
+      <article>
+        <p className="line-clamp-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit assumenda libero voluptates neque? Laudantium perspiciatis excepturi tenetur, fuga quidem repellendus voluptatem suscipit beatae reprehenderit esse quis doloribus a veniam possimus?
+        </p>
+        <p className="underline">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, dolorem.</p>
+        <p className="line-through">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="uppercase">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="lowercase">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="capitalize">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum?</p>
+        <p className="text-[rgba(235,34,34,1)]">Hello Tailwind </p> 
+
+
+
+
+      </article>
     </div>
   );
 };
