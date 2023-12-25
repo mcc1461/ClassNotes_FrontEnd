354bae3f6bf6557b7a8c449ecb18c731b4723f1d
mcc1461 <mcoskuncelebi@gmail.com>
2023-12-25 23:14:27 +0300

    note details modified

--- react/class-notes/mcc_rc09_lifecycle-useEffect/src/App.js
+++ react/class-notes/mcc_rc09_lifecycle-useEffect/src/App.js
@@ -1,32 +1,45 @@
-import React from 'react';
-import UseEffectLearn from './components/UseEffectLearn';
-import './App.css';
-import Merhaba from './components/UseState';
+// Importing React and the useState hook from the React library.
+import React, { useState } from "react"; 
+// Importing the CSS styles specific to this application.
+import "./App.css"; 
+// Importing Bootstrap's minified CSS for styling.
+import "bootstrap/dist/css/bootstrap.min.css"; 
 
+// Declaring a functional component named Mcc.
+const Mcc = () => { 
+  // Initializing a state variable 'count' with a default value of 0 and a setter 'setCount'.
+  const [count, setCount] = useState(0); 
+  // Starting the JSX return block.
+  return ( 
+    // Using "React Fragment" to group multiple elements without adding an extra node to the DOM.
+    <> 
+      {/* Adding Bootstrap-styled div for the counter. */}
+      <div className="m-5 rounded bg-warning w-25 d-flex justify-content-between text-dark"> 
+        {/* Adding button to increment the count, styled with Bootstrap. */}
+        <button className="btn btn-success" onClick={() => setCount(count + 1)}> 
+          inc 
+        </button>
+        {/* Displaying the current count value. */}
+        <h1> {count} </h1> 
+        {/* Adding button to decrement the count, styled with Bootstrap. */}
+        <button 
+          className="btn btn-danger"
+          // Only decrementing the count if it's greater than 0. (preventing negative values)
+          onClick={() => count > 0 && setCount(count - 1)} 
+        >
+          dec 
+        </button>
+      </div>
+      {/* Using a styled div with a friendly message. (Otherwise with one element fragment does not work) */} 
+      <div className="p-5 m-4 fw-bold fs-2 text-dark">Happy Coding!</div> 
+    </> 
+  );
+};
 
-function App() {
-
-  return (
-
-    <>
-
-  <div className="uel">
-
-  <UseEffectLearn />
-  
-  </div>
-  <Merhaba />
-  
-  <h1 style={
-    {color:"red", padding:"20px", backgroundColor:"yellow", width:"200px"}
-  }>
-  mcc1461
-  </h1>
-  </>
-    )
+// Defining the main App component.
+export default function App() { 
+  // Rendering the Mcc component within the App component.
+  return <Mcc />; 
 }
 
 
-  
-
-export default App;
--- react/class-notes/mcc_rc09_lifecycle-useEffect/src/components/UseState.jsx
+++ react/class-notes/mcc_rc09_lifecycle-useEffect/src/components/UseState.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 
 const Merhaba = () => {
 
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-warning w-25 d-flex justify-content-between">
+      
+<div className="m-5 rounded bg-blue bg-warning w-25 d-flex justify-content-between text-dark">
       <button className="btn btn-success" onClick={()=> setCount(count+1)}>inc</button>
-      <h1>{count}</h1>
+      <h1> {count}  </h1>
       <button className="btn btn-danger" onClick={()=> count > 0 && setCount(count-1)}>dec</button>
     </div>
+
   )
 }
 
