cbee23b5d7dbc599a5c9030145f0dabbdbd3a6cc
mcc1461 <mcoskuncelebi@gmail.com>
2024-01-04 20:16:55 +0300

    Todo app part added

--- react/class-notes/mcc_rc20_redux-intro/redux-intro-starter/src/App.js
+++ react/class-notes/mcc_rc20_redux-intro/redux-intro-starter/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Counter from "./components/counter/Counter";
-// import Todo from "./components/todo/Todo";
+import Todo from "./components/todo/Todo";
 import { Provider } from "react-redux";
 import { storage } from "./store/store";
 
@@ -10,7 +10,7 @@ function App() {
     <div className="app">
       <Provider store={storage}>
         <Counter />
-        {/* <Todo /> */}
+        <Todo />
       </Provider>
     </div>
   );
