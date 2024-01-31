d1b91525e494a44f0d119d0dae4fd46e799bc87c
mcc1461 <mcoskuncelebi@gmail.com>
2024-01-31 20:23:29 +0300

    nextjs notes

---
+++ nextjs/class-notes/nc01-intro/app/(private)/dashboard/users/[userId]/page.jsx
@@ -0,0 +1,24 @@
+import GoBackButton from "@/components/GoBack";
+// import { useParams } from "next/navigation";
+import React from "react";
+
+const UserDetail = ({ params }) => {
+  //* client componentler için useParam() hooku kullanabiliriz
+  // const {userId}= useParams()
+  //   console.log(params);
+  const { userId } = params;
+  return (
+    <div>
+      <h1 className="text-2xl text-center">UserDetail</h1>
+      <p>user - {userId}</p>
+      <GoBackButton />
+    </div>
+  );
+};
+
+export default UserDetail;
+
+export async function generateStaticParams() {
+  const userArr = [1, 2, 3, 4];
+  return userArr.map((userId) => ({ userId: userId.toString() }));
+}
